06f2d2e3f953faa9919c966637a53cd335e1089e
Yemi Ogundairo <62849248+SodiqOgundairo@users.noreply.github.com>
2025-10-03 23:59:54 +0100

    db test

--- db.py
+++ db.py
@@ -1,7 +1,16 @@
 import sqlite3
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)')
+c.execute("""
+          CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)
+          """)
+
+c.execute("""
+          INSERT INTO users VALUES 
+          (1, 'Alice', 30),
+            (2, 'Bob', 25),
+            (3, 'Charlie', 35)
+          """)
 c.close()
 conn.commit()
 conn.close()
